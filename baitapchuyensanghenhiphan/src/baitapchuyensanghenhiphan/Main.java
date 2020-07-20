3c031d661d4dcfc4d9ab2ad8b482f1af85ef036c
Phamtung1802 <66355069+Phamtung1802@users.noreply.github.com>
2020-07-20 22:39:38 +0700

    update bai tap so nhi phan

---
+++ baitapchuyensanghenhiphan/src/baitapchuyensanghenhiphan/Main.java
@@ -0,0 +1,8 @@
+package baitapchuyensanghenhiphan;
+
+public class Main {
+    public static void main(String[] args) {
+        MyStack list=new MyStack(12145);
+        System.out.println("So Binary La: "+ list.convertDecimal());
+    }
+}
