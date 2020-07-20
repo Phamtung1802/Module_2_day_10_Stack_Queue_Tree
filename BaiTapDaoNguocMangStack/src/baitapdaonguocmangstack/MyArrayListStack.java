f859cfd6aff51439dd952f5c44fd97c3ae01f257
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-20 16:55:01 +0700

    Update MyArrayListStack.java

--- BaiTapDaoNguocMangStack/src/baitapdaonguocmangstack/MyArrayListStack.java
+++ BaiTapDaoNguocMangStack/src/baitapdaonguocmangstack/MyArrayListStack.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyArrayListStack<E> {
-   private ArrayList<E> stack;
+    private ArrayList<E> stack;
 
-   public MyArrayListStack(){
-       stack=new ArrayList<E>();
-   }
+    public MyArrayListStack() {
+        stack = new ArrayList<E>();
+    }
 
     public void push(E element) {
-        this.stack.add(0,element);
+        this.stack.add(0, element);
     }
 
     public E pop() {
-        if(isEmpty())
+        if (isEmpty())
             throw new EmptyStackException();
-        E obj=this.stack.get(this.stack.size()-1);
-        this.stack.remove(this.stack.size()-1);
+        E obj = this.stack.get(this.stack.size() - 1);
+        this.stack.remove(this.stack.size() - 1);
         return obj;
     }
 
@@ -29,9 +29,10 @@ public class MyArrayListStack<E> {
         return false;
     }
 
-    public ArrayList<E> getAll(){
-       return stack;
+    public ArrayList<E> getAll() {
+        return stack;
     }
+
     public E get(int a) {
         return stack.get(a);
     }
