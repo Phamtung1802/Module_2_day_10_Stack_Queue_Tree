744819c99797bd5d0f02d3d734942ac4b1549cb2
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-20 10:29:26 +0700

    add bai tap array stack list

--- BaiTapDaoNguocMangStack/src/baitapdaonguocmangstack/Main.java
+++ BaiTapDaoNguocMangStack/src/baitapdaonguocmangstack/Main.java
@@ -1,8 +1,29 @@
 package baitapdaonguocmangstack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         MyArrayListStack<Integer> intList=new MyArrayListStack<Integer>();
-        
+        MyArrayListStack<String> strList=new MyArrayListStack<String>();
+
+        for(int i = 0; i<5;i++){
+            intList.push(i);
+            System.out.println("added: "+intList.get(0));
+        }
+        System.out.println("List Before "+intList.getAll());
+        Collections.reverse(intList.getAll());
+        System.out.println("List After "+intList.getAll());
+
+        for(Integer i=0;i<5;i++){
+            strList.push("a"+i.toString());
+            System.out.println("added: "+strList.get(0));
+        }
+        System.out.println("List Before "+strList.getAll());
+        Collections.reverse(strList.getAll());
+        System.out.println("List After "+strList.getAll());
+
     }
 }
--- BaiTapDaoNguocMangStack/src/baitapdaonguocmangstack/MyArrayListStack.java
+++ BaiTapDaoNguocMangStack/src/baitapdaonguocmangstack/MyArrayListStack.java
@@ -3,6 +3,33 @@ package baitapdaonguocmangstack;
 import java.util.ArrayList;
 
 public class MyArrayListStack<E> {
-    ArrayList<E> stack=new ArrayList<E>();
+   private ArrayList<E> stack;
+
+   public MyArrayListStack(){
+        stack=new ArrayList<E>();
+   }
+
+    public void push(E element) {
+        this.stack.add(0,element);
+    }
+
+    public void pop(E element) {
+        this.stack.remove(0);
+    }
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<E> getAll(){
+       return stack;
+    }
+
+    public E get(int a){
+        return stack.get(a);
+    }
 
 }
